378195aaa33f8eeb2aecc1e5f07ec3c232aef63e
Gugol-Ram <ramiro.heredia@mi.unc.edu.ar>
2023-11-30 15:40:42 -0300

    up

--- drivers/src/Redux/Actions/indexActions.js
+++ drivers/src/Redux/Actions/indexActions.js
@@ -15,12 +15,14 @@ import {
   POST_DRIVER,
   DELETE_DRIVER,
 } from "./actionTypes";
+const URL = "http://localhost";
+const PORT = process.env.PORT || 3001;
 
 // ACA ES LA RELACION FRONT-BACK,CLAVE!!
 
 export const getDrivers = () => {
   return async function (dispatch) {
-    const response = await axios.get("http://localhost:3001/drivers");
+    const response = await axios.get(`${URL}:${PORT}/drivers`);
     return dispatch({
       type: GET_DRIVERS,
       payload: response.data,
@@ -31,9 +33,7 @@ export const getDrivers = () => {
 export const getDriverByName = (name) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(
-        `http://localhost:3001/drivers/name/${name}`
-      );
+      const response = await axios.get(`${URL}:${PORT}/drivers/name/${name}`);
       return dispatch({
         type: GET_DRIVER_BY_NAME,
         payload: response.data,
@@ -50,7 +50,7 @@ export const getDetailDriver = (id) => {
       type: GET_DRIVER_BY_ID_REQUEST,
     });
     try {
-      const response = await axios.get(`http://localhost:3001/drivers/${id}`);
+      const response = await axios.get(`${URL}:${PORT}/drivers/${id}`);
       return dispatch({
         type: GET_DRIVER_BY_ID,
         payload: response.data,
@@ -80,7 +80,7 @@ export const clearDetails = (id) => {
 export const getTeams = () => {
   return async function (dispatch) {
     try {
-      const response = await axios.get("http://localhost:3001/teams");
+      const response = await axios.get(`${URL}:${PORT}/teams`);
       return dispatch({
         type: GET_TEAMS,
         payload: response.data,
@@ -124,7 +124,7 @@ export const teamComb = (value) => {
 export const postDriver = (data) => {
   return async function (dispatch) {
     try {
-      const response = await axios.post("http://localhost:3001/drivers/", data);
+      const response = await axios.post(`${URL}:${PORT}/drivers`, data);
       alert("Driver Created Successfully!");
       return response;
     } catch (error) {
@@ -152,9 +152,7 @@ export const refreshForm = () => {
 export const deleteDriver = (id) => {
   return async function (dispatch) {
     try {
-      const response = await axios.delete(
-        `http://localhost:3001/drivers/${id}`
-      );
+      const response = await axios.delete(`${URL}:${PORT}/drivers/${id}`);
       return dispatch({
         type: DELETE_DRIVER,
         payload: response.data,
